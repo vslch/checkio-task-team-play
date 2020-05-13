3bd33cac5a55649755f30d551b4fda99df4ab4c0
Vasiliy Chibiliaev <chibiliaev23@gmail.com>
2020-05-13 17:30:05 +0300

    Modified referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -38,8 +38,7 @@ api.add_listener(
     CheckiOReferee(
         tests=TESTS,
         function_name={
-            "python": "sum_two",
-            "js": "sumTwo"
+            "python": "max_score"
         },
         cover_code={
             'python-3': cover_codes.unwrap_args,
